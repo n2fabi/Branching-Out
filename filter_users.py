fdf2f7e261574a3b56fca037409a6e8e9ca6318d
Fabian Wohlgemuth <fabi@mac.speedport_w_724v_typ_a_05011603_06_003>
2025-07-02 10:30:34 +0200

    added logic to filter by age

--- filter_users.py
+++ filter_users.py
@@ -10,12 +10,26 @@ def filter_users_by_name(name):
     for user in filtered_users:
         print(user)
 
+def  filter_users_by_age(age_min, age_max):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    filtered_users = [user for user in users if age_min < user["age"] < age_max]
+
+    for user in filtered_users:
+        print(user)
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, only 'name' is supported): ").strip().lower()
+    filter_option = input("What would you like to filter by? (Currently, only 'name' and 'age' are supported): ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
+
+    if filter_option == "age":
+        age_min = int(input("Enter a minimum age to filter users: ").strip())
+        age_max = int(input("Enter a maximum age to filter users: ").strip())
+        filter_users_by_age(age_min, age_max)
+
     else:
         print("Filtering by that option is not yet supported.")
