4006edef167b663b882aa389fa31ade003a4e0e8
Fabian Wohlgemuth <fabi@mac.speedport_w_724v_typ_a_05011603_06_003>
2025-07-02 10:44:47 +0200

    added feature filter by email

--- filter_users.py
+++ filter_users.py
@@ -19,17 +19,30 @@ def  filter_users_by_age(age_min, age_max):
     for user in filtered_users:
         print(user)
 
+def filter_users_by_email(email):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    filtered_users = [user for user in users if user["email"].lower() == email.lower()]
+
+    for user in filtered_users:
+        print(user)
+
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, only 'name' and 'age' are supported): ").strip().lower()
+    filter_option = input("What would you like to filter by? (Currently, only 'name','email' and 'age' are supported): ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
 
-    if filter_option == "age":
+    elif filter_option == "age":
         age_min = int(input("Enter a minimum age to filter users: ").strip())
         age_max = int(input("Enter a maximum age to filter users: ").strip())
         filter_users_by_age(age_min, age_max)
+
+    elif filter_option == "email":
+        email_to_search = input("Enter a email to filter users: ").strip()
+        filter_users_by_name(email_to_search)
 
     else:
         print("Filtering by that option is not yet supported.")
